626b26077433b0da8e07c66402d85ac894d1f454
Ivan Cardillo <ivan.cardillo-cic-it@ibm.com>
2019-12-14 12:22:46 +0100

    Serve init

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require('./app');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('Listening on port: ' + port);
+    //console.log(process.env.USER_ID);
+});
